4ab3f7c6acf3ab3211120af5efc0ac3ca1a73cf2
Steven DeLoach <sfdeloach@gmail.com>
2017-04-27 23:38:48 -0400

    more calc table work

--- public/scripts/calcScript.js
+++ public/scripts/calcScript.js
@@ -1,22 +1,29 @@
 /*jslint devel: true*/
 /*globals $: false*/
+/*globals benchTables: false*/
 /*globals bodyTables: false*/
+/*globals flexTables: false*/
 
 $(document).ready(function () {
     'use strict';
     var sex = $('.sex'),
         dob = $('.dob'),
         evalDate = $('.eval-date'),
+        weight = $('.weight'),
         age = $('.age'),
-        //
+        bench = $('.bench'),
+        benchResult = $('.bench-result'),
         body = $('.body'),
         bodyResult = $('.body-result'),
-        //
+        flex = $('.flex'),
+        flexResult = $('.flex-result'),
         sumParticipants = evalDate.length,
         i;
     
     for (i = 0; i < sumParticipants; i += 1) {
         age[i].innerHTML = Math.floor((new Date(evalDate[i].innerHTML).getTime() - new Date(dob[i].innerHTML).getTime()) / (1000 * 60 * 60 * 24 * 365.25));
+        benchResult[i].innerHTML = benchTables.lookup(sex[i].innerHTML, age[i].innerHTML, bench[i].innerHTML / weight[i].innerHTML);
         bodyResult[i].innerHTML = bodyTables.lookup(sex[i].innerHTML, age[i].innerHTML, body[i].innerHTML);
+        flexResult[i].innerHTML = flexTables.lookup(sex[i].innerHTML, age[i].innerHTML, flex[i].innerHTML);
     }
 });
